eb4c680a722f7bc61e8786c4ae6070b9973edca4
João Victor Olivo <jvsoxg@gmail.com>
2025-03-05 11:02:23 -0300

    Jumpable character

--- out/script.js
+++ out/script.js
@@ -1,12 +1,53 @@
 "use strict";
 const canvas = document.getElementById('main-canvas');
 const ctx = canvas.getContext('2d');
-function desenharPersonagem() {
-    ctx.fillStyle = 'brown';
-    ctx.fillRect(100, 25, 50, 50);
+class Character {
+    x;
+    y;
+    width;
+    height;
+    jumping = false;
+    yspeed = 0;
+    gravity = 1;
+    constructor(x, y, width, height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+}
+const player = new Character(100, canvas.height - 50, 50, 50);
+document.addEventListener('keydown', (event) => {
+    if (event.key === " " && !player.jumping) {
+        console.log('Jumping');
+        player.yspeed = 15;
+        player.jumping = true;
+    }
+});
+function drawCharacter() {
+    ctx.fillStyle = 'white';
+    ctx.fillRect(player.x, player.y, player.width, player.height);
+}
+function jump() {
+    if (!player.jumping)
+        return;
+    if (player.y > canvas.height - player.height) {
+        player.jumping = false;
+        player.y = canvas.height - player.height;
+        return;
+    }
+    player.yspeed -= player.gravity;
+    player.y -= player.yspeed;
+}
+function updateCharacter() {
+    jump();
+    player.y = Math.max(0, player.y);
 }
 function loop() {
-    desenharPersonagem();
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawCharacter();
+    updateCharacter();
+    requestAnimationFrame(loop);
 }
 loop();
 //# sourceMappingURL=script.js.map
--- src/script.ts
+++ src/script.ts
@@ -1,13 +1,62 @@
 const canvas = document.getElementById('main-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d')!;
 
-function desenharPersonagem() {
-    ctx.fillStyle = 'brown';
-    ctx.fillRect(100, 25, 50, 50);
+class Character {
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+    jumping = false;
+    yspeed = 0;
+
+    gravity = 1;
+
+    constructor(x: number, y: number, width: number, height: number) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+}
+
+const player = new Character(100, canvas.height - 50, 50, 50);
+
+document.addEventListener('keydown', (event) => {
+    if (event.key === " " && !player.jumping) {
+        console.log('Jumping');
+        player.yspeed = 15;
+        player.jumping = true;
+    }
+})
+
+function drawCharacter() {
+    ctx.fillStyle = 'white';
+    ctx.fillRect(player.x, player.y, player.width, player.height);
+}
+
+function jump() {
+    if (!player.jumping) return;
+
+    if (player.y > canvas.height - player.height) {
+        player.jumping = false;
+        player.y = canvas.height - player.height;
+        return;
+    }
+
+    player.yspeed -= player.gravity;
+    player.y -= player.yspeed; 
+}
+
+function updateCharacter() {
+    jump();
+    player.y = Math.max(0, player.y);
 }
 
 function loop() {
-    desenharPersonagem();
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawCharacter();
+    updateCharacter();
+    requestAnimationFrame(loop);
 }
 
 loop();
